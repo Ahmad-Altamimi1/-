36841053f0b9eb9c2b84c0d02e03c014fd947bdb
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-19 00:00:13 +0300

    r

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Post;
+use App\Models\groups;
 use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
-    public function index(){
-        $posts = Post::with(['tag', 'group'])->take(4)->get();
-         return response()->json(['posts'=>$posts,], 200);
+    public function index()
+    {
+        $totalGroups = count(groups::all());
+
+        $randomTopics = [
+            rand(1, $totalGroups),
+            rand(1, $totalGroups),
+        ];
+
+        $posts = Post::whereIn('TOPIC', $randomTopics)->with(['tag', 'group'])->take(4)->get();
+
+        return response()->json($posts, 200);
     }
 }
+
