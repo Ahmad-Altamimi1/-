0c57eae37aaa64166d4303a236e4d387647e4028
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-19 00:19:50 +0300

    r

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,12 +10,13 @@ class APIController extends Controller
     public function index(){
         $totalGroups = count(groups::all());
 
-        $randomTopics = [
-            rand(90, $totalGroups),
-            rand(90, $totalGroups),
-        ];
+
 
         function postcheck (){
+            $randomTopics = [
+                rand(90, $totalGroups),
+                rand(90, $totalGroups),
+            ];
             $posts = Post::whereIn('TOPIC', $randomTopics)->with(['tag', 'group'])->take(4)->get();
             if (count($posts)<1) {
 
