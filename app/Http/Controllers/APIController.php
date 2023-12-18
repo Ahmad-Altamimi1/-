606ce1a9812122ead70a0dbb4a99873a772a4457
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-18 20:04:56 +0300

    r

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class APIController extends Controller
 {
     public function index(){
-        $posts=Post::all();
+        $posts=Post::take(40);
          return response()->json($posts, 200);
     }
 }
