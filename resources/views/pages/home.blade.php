0bb18c143717a6cf93a3533d38fc74f63cc94fc1
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-28 21:19:49 +0300

    tm

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -240,14 +240,14 @@ body{
 
         <!-- SLIDER AREA START (slider-6) -->
 
-                      @if ($havevideo)
+                      @if ($havevideo || 1)
     <div id="halfslider" style="width: 70%">
          <div class="ltn__slider-area ltn__slider-4 position-relative">
         <div class="ltn__slide-one-active----- slick-slide-arrow-1----- slick-slide-dots-1----- arrow-white----- ltn__slide-animation-active">
 
             <!-- HTML5 VIDEO -->
             <video autoplay muted loop id="myVideo">
-                <source src="../{{ $slidercontent->video }}" type="video/mp4">
+                <source src="../{{ asset('uploads/Untitled video - Made with Clipchamp (1).mp4') }}" type="video/mp4">
             </video>
 
             <!-- YouTube VIDEO -->
@@ -266,7 +266,7 @@ body{
                                 <div class="slide-item-info">
                                     <div class="slide-item-info-inner ltn__slide-animation">
                                         <h6 class="slide-sub-title white-color animated">مرحبا بك في إنْجابيّ</h6>
-                                        <h1 class="slide-title text-uppercase white-color animated ">{{ $slidercontent->description }}</h1>
+                                        {{-- <h1 class="slide-title text-uppercase white-color animated ">{{ $slidercontent->description }}</h1> --}}
                                     </div>
                                 </div>
                             </div>
