79fc15f18c9d91e27b91c0b9cd7a0261b7383046
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-29 20:34:13 +0300

    tm

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,125 +4,10 @@
 
 
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 
 
 
     <style>
-          #my-slider {
-        width: 100%;
-        margin: 0 auto;
-        position: relative;
-        padding: 0 100px;
-    }
-.slider-inner{
-    background-color: #d54368;
-
-}
-    .slick-slide img {
-        width: 100%;
-        height: auto;
-    }
-
-    .slide-section {
-        position: relative;
-    }
-
-    .image-section, .description-section {
-        width: 60%; /* Adjust the width as needed */
-        height: 100%;
-        float: left;
-    }
-
-    .image-section img {
-        width: 100%;
-    aspect-ratio: 2/3;
-    height: 100%;
-
-    }
-
-    .description-section {
-        background-color: #fff;
-        padding: 30px;
-        border-radius: 10px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        left: 60%; /* Adjust the left position */
-        width: 40%; /* Adjust the width as needed */
-        text-align: center;
-    }
-
-    .description-section h2 {
-        margin-bottom: 10px;
-    }
-
-    .description-section p {
-        font-size: 16px;
-    }
-
-    .slick-prev, .slick-next {
-        font-size: 24px;
-        color: #fff;
-        background-color: #333;
-        width: 40px;
-        height: 40px;
-        line-height: 40px;
-        text-align: center;
-        border-radius: 50%;
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        z-index: 1;
-        cursor: pointer;
-    }
-
-    .slick-prev {
-        left: 0;
-    }
-    #my-slider {
-    width: 100%;
-    margin: 0 auto;
-    position: relative;
-    padding: 0 100px; /* Add padding 100px from right and left */
-    height: 50vh; /* Set the height to 50vh */
-}
-
-/* ... (previous styles remain unchanged) ... */
-
-.slick-prev, .slick-next {
-    font-size: 24px;
-    color: #fff;
-    background-color: #333;
-    width: 40px;
-    height: 40px;
-    line-height: 40px;
-    text-align: center;
-    border-radius: 50%;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    z-index: 1;
-    cursor: pointer;
-    opacity: 0.7; /* Add opacity for better visibility */
-}
-
-.slick-prev:hover, .slick-next:hover {
-    opacity: 1; /* Increase opacity on hover */
-}
-
-.slick-prev {
-    left: 500px; /* Adjust the left position */
-}
-
-.slick-next {
-    left: 500px;/* Adjust the right position */
-}
-
-    .slick-next {
-        right: 0;
-    }
         /* {{-- /// START artical css//// --}} */
         article {
   --img-scale: 1.001;
@@ -348,41 +233,179 @@ body{
     /* direction: rtl; */
 }
         </style>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
-
         </div>
-        <div id="my-slider">
-            <div class="slider-inner">
-                <!-- Slide 1 -->
-                <div class="item">
-                    <div class="slide-section">
-                        <div class="image-section">
-                            <img src="{{ asset('https://todaysparent.mblycdn.com/uploads/tp/2014/12/blanket-baby-article.jpg') }}" alt="Slide 1 Image">
-                        </div>
-                        <div class="description-section">
-                            <h2>Image 1 Title</h2>
-                            <p>Description of Image 1.</p>
-                        </div>
-                    </div>
-                </div>
+    {{-- <div class="ltn__utilize-overlay"></div> --}}
 
-                <!-- Slide 2 -->
-                <div class="item">
-                    <div class="slide-section">
-                        <div class="image-section">
-                            <img src="{{ asset('https://todaysparent.mblycdn.com/uploads/tp/2014/12/blanket-baby-article.jpg') }}" alt="Slide 2 Image">
+
+
+        <!-- SLIDER AREA START (slider-6) -->
+
+                      @if ($havevideo )
+    <div id="halfslider" style="width: 70%">
+         <div class="ltn__slider-area ltn__slider-4 position-relative">
+        <div class="ltn__slide-one-active----- slick-slide-arrow-1----- slick-slide-dots-1----- arrow-white----- ltn__slide-animation-active">
+
+            <!-- HTML5 VIDEO -->
+            <video autoplay muted loop id="myVideo">
+                <source src="../{{ asset('uploads/Untitled video - Made with Clipchamp (1).mp4') }}" type="video/mp4">
+            </video>
+
+            <!-- YouTube VIDEO -->
+            <!-- <div class="ltn__youtube-video-background">
+                <iframe frameborder="0" height="100%" width="100%"
+                  src="https://www.youtube.com/embed/9DYl-nKrQ0k?playlist=9DYl-nKrQ0k&loop=1&controls=0&showinfo=0&autoplay=1&mute=1">
+                </iframe>
+            </div> -->
+
+            <!-- ltn__slide-item -->
+            <div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-7 bg-image--- bg-overlay-theme-black-30" data-bs-bg="img/slider/41.jpg">
+                <div class="ltn__slide-item-inner text-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 align-self-center">
+                                <div class="slide-item-info">
+                                    <div class="slide-item-info-inner ltn__slide-animation">
+                                        <h6 class="slide-sub-title white-color animated">مرحبا بك في إنْجابيّ</h6>
+                                        {{-- <h1 class="slide-title text-uppercase white-color animated ">{{ $slidercontent->description }}</h1> --}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="description-section">
-                            <h2>Image 2 Title</h2>
-                            <p>Description of Image 2.</p>
-                        </div>
-                        <div class="slick-prev">&#9664; ddd</div>
-                        <div class="slick-next">&#9654; dddggg</div>
                     </div>
                 </div>
             </div>
-            <div class="slick-prev">&#9664; ddd</div>
-            <div class="slick-next">&#9654; dddggg</div>
+        </div>
+    </div>
+    </div>
+@endif
+<div class="allheader" style=" display: grid;
+grid-template-columns: 65% 30%;
+gap: 2%;
+padding:0 15%">
+    <div id="halfslider" style="width: 100%;padding-left:30px">
+
+        <div class="ltn__slider-area ltn__slider-3 ltn__slider-6 ">
+            <div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1 arrow-white---">
+                <!-- ltn__slide-item  -->
+                      @if (!$havevideo)
+
+                @foreach ($slidercontent as $slider)
+
+
+
+                <div class="ltn__slide-item ltn__slide-item-8 text-color-white---- bg-image bg-overlay-theme-black-80---" data-bs-bg="{{ $slider->posts->IMG }}" style="width: 50%;">
+                    <div class="ltn__slide-item-inner" style="border-radius: 64px;">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12 align-self-center" style="    text-align: right;
+                                display: flex;
+                                justify-content: right;">
+                                    <div class="slide-item-info" style="flex: 1">
+                                        <div class="slide-item-info-inner ltn__slide-animation">
+                                            <div class="slide-item-info">
+                                                <div class="slide-item-info-inner ltn__slide-animation">
+                                                    <h1 class="slide-title animated ">اهلا بك في إنْجابيّ </h1>
+                                                    <h6 class="slide-sub-title ltn__body-color slide-title-line animated">{{ $slider->posts->group->TITLE }}</h6>
+                                                    <div class="slide-brief animated">
+                                                        <p>{{ $slider->description }}</p>
+                                                    </div>
+                                                    <div class="btn-wrapper animated">
+                                                    <a href="{{ route('ShoWarticle', ['id' => $slider->posts->id]) }}" class="theme-btn-1 btn btn-round">أقرأ المقال</a>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- <div class="slide-item-img">
+                                        <img src="pages/img/slider/41-1.png" alt="#">
+                                        <span class="call-to-circle-1"></span>
+                                    </div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            @endif
+
+
+
+
+
+            </div>
+            <section class="ftco-section ftco-no-pb ftco-no-pt" style="">
+
+        </div>
+    </div>
+    @if (!$havevideo)
+    <div class="container">
+        <!-- Popular Post Widget -->
+        <div class="row">
+
+            <div class="col-md-5 order-md-last" style="width:40%" >
+            <div class="login-wrap p-4 p-md-5"  style="    WIDTH: 540PX;">
+
+                            <div class="widget ltn__popular-post-widget" >
+                                <div class="flex" style="justify-content: justify-content:space-around;">
+                                    <h2 class="ltn__widget-title w-50" data-mode="popular" id="popularPostsTitle" style="cursor: pointer">المقالات المشهوره</h2>
+                                    <h2 class="ltn__widget-title  w-50 " data-mode="recent" style="background: white;
+                                    color: black;
+                                    border-radius: 8px;
+                                    text-align: center;cursor: pointer" >المقالات الحديثه</h2>
+
+
+                                </div>
+                                <div class="ajaxul">
+                                <ul class="flex" style="display:grid; grid-template-columns: 50% 50%;gap: 16px;">
+                                    @foreach ($recentposts as $recentpost)
+
+                                    <li>
+                                        <div class="popular-post-widget-item " style="background: white;
+
+                                        display: flex;
+                                        flex-direction: column;
+                                        align-items: center;
+                                        padding: 10px 0 0 0;
+                                        border-radius: 16px;
+                                        width: 100%;
+height: 100%;">
+                                            <div class="popular-post-widget-img" >
+                                                <a href="{{ route('ShoWarticle', ['id' => $recentpost->id]) }}"><img src="{{ asset($recentpost->IMG) }}" alt="#" style=""></a>
+
+                                            </div>
+                                            <div class="popular-post-widget-brief">
+                                                <div class="ltn__blog-meta">
+                                                    <ul>
+                                                        <span class=" " style="width: 100%;
+                                                        display: inline-block;
+                                                        text-align: center;"><a href="{{ route('ShoWarticle', ['id' => $recentpost->id]) }}"> {{ $recentpost->TITLE }}</a></span>
+                                                        <li>
+                                                            <span></span>
+                                                            {{-- <span>{{ \Carbon\Carbon::parse($recentpost->DATE_SCHEDULER)->locale('ar')->isoFormat('MMM DD, YYYY') }}</span> --}}
+                                                        </li>
+
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    @endforeach
+
+                                </ul>
+                            </div>
+                            </div>
+                            @endif
+    </div>
+    </div>
+    </div>
+    </section>
+        </div>
+        </div>
+    </div>
+
+        </div>
+        </div>
         </div>
            {{-- <div class="row pt-60 pb-40" style="width: 100%;">
                     <div class="col-lg-12">
@@ -711,38 +734,6 @@ use App\Models\poststags;
 
 
     @endsection
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            // Initialize Slick Slider
-            $('#my-slider .slider-inner').slick({
-                dots: false,
-                arrows: false,
-                autoplay: true,
-                autoplaySpeed: 5000,
-                fade: true,
-                speed: 1000,
-                responsive: [
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            dots: false,
-                        }
-                    }
-                ]
-                // Add more configuration options as needed
-            });
-
-            // Custom navigation arrows
-            $('#my-slider .slick-prev').click(function(){
-                $('#my-slider .slider-inner').slick('slickPrev');
-            });
-
-            $('#my-slider .slick-next').click(function(){
-                $('#my-slider .slider-inner').slick('slickNext');
-            });
-        });
-    </script>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
