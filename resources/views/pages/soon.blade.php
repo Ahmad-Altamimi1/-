708cf9868797440996d1d1dda94055f7c0e01ff2
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-29 19:33:10 +0300

    tm

--- resources/views/pages/soon.blade.php
+++ resources/views/pages/soon.blade.php
@@ -8,16 +8,36 @@
         margin: 0 auto;
     }
 
-    .info-section, .testimonial-section {
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    .slick-slide img {
+        width: 100%;
+        height: auto;
+    }
+
+    .testimonial-section {
+        position: relative;
     }
 
     .testimonial-box {
+        background-color: #fff;
+        padding: 30px;
+        border-radius: 10px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        max-width: 600px;
         text-align: center;
+    }
+
+    .testimonial-box blockquote {
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+
+    .testimonial-box cite {
+        font-style: italic;
+        font-size: 16px;
     }
 </style>
 
@@ -27,6 +47,7 @@
     <div class="slider-inner">
         <!-- Slide 1: Informational Part -->
         <div class="item">
+            <img src="{{ asset('https://todaysparent.mblycdn.com/uploads/tp/2014/12/blanket-baby-article.jpg') }}" alt="Slide 1 Image">
             <div class="info-section">
                 <h2>Informational Title</h2>
                 <p>Some informative text here.</p>
@@ -37,9 +58,9 @@
 
         <!-- Slide 2: Image with Testimonial -->
         <div class="item">
+            <img src="{{ asset('https://todaysparent.mblycdn.com/uploads/tp/2014/12/blanket-baby-article.jpg') }}" alt="Slide 2 Image">
             <div class="testimonial-section">
                 <div class="testimonial-box">
-                    <img src="{{ asset('https://todaysparent.mblycdn.com/uploads/tp/2014/12/blanket-baby-article.jpg') }}" alt="Testimonial Image">
                     <blockquote>
                         <p>Testimonial text goes here.</p>
                         <cite>Testimonial Author</cite>
@@ -57,10 +78,20 @@
     document.addEventListener('DOMContentLoaded', function () {
         // Initialize Slick Slider
         $('#my-slider .slider-inner').slick({
-            // Slick Slider options go here
             dots: true,
-            infinite: true,
-            speed: 500,
+            arrows: false, // Remove arrows
+            autoplay: true,
+            autoplaySpeed: 5000,
+            fade: true, // Enable fade effect
+            speed: 1000, // Set transition speed
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        dots: false, // Hide dots on smaller screens
+                    }
+                }
+            ]
             // Add more configuration options as needed
         });
     });
