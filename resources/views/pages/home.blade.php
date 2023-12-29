16aea6bb37b1d912f959b6f8ce0302c27438ae19
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-30 02:16:34 +0300

    tm

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -283,7 +283,7 @@ body{
 grid-template-columns: 65% 30%;
 gap: 2%;
 ">
-    <div id="halfslider" style="width: 100%;padding-left:30px">
+    <div id="halfslider" style="width: 100%;">
 
         <div class="ltn__slider-area ltn__slider-3 ltn__slider-6 ">
             <div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1 arrow-white---">
@@ -295,7 +295,7 @@ gap: 2%;
 
 
                 <div class="ltn__slide-item ltn__slide-item-8 text-color-white---- bg-image bg-overlay-theme-black-80---" data-bs-bg="{{ $slider->posts->IMG }}" style="width: 50%;">
-                    <div class="ltn__slide-item-inner" style="border-radius: 64px;">
+                    <div class="ltn__slide-item-inner" style="border-radius: 0  64px 0;">
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-12 align-self-center" style="    text-align: right;
