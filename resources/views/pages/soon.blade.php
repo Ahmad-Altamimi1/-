6f120b6b25498c248d359bc716963d90ec4398b6
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-29 19:29:16 +0300

    tm

--- resources/views/pages/soon.blade.php
+++ resources/views/pages/soon.blade.php
@@ -3,24 +3,24 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <style>
     /* Example CSS styles (adjust as needed) */
-#my-slider {
-    width: 100%;
-    margin: 0 auto;
-}
+    #my-slider {
+        width: 100%;
+        margin: 0 auto;
+    }
 
-.info-section, .testimonial-section {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #fff;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+    .info-section, .testimonial-section {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
 
-.testimonial-box {
-    text-align: center;
-}
-
+    .testimonial-box {
+        text-align: center;
+    }
 </style>
+
 <!-- Add Slick Slider Theme CSS if needed -->
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 <div id="my-slider">
@@ -50,14 +50,17 @@
     </div>
 </div>
 
-
 @endsection
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
-        var slider = new SmartSlider({
-            selector: '#my-slider',
-            duration: 500, // Set your preferred duration
+        // Initialize Slick Slider
+        $('#my-slider .slider-inner').slick({
+            // Slick Slider options go here
+            dots: true,
+            infinite: true,
+            speed: 500,
             // Add more configuration options as needed
         });
     });
