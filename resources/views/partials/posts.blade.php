0b1abefe16430b2b5bf3d21693fb733e63ff208e
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2023-12-31 10:32:07 +0300

    tm

--- resources/views/partials/posts.blade.php
+++ resources/views/partials/posts.blade.php
@@ -1,24 +1,21 @@
-<!-- partials/posts.blade.php -->
+@foreach ($recentposts as $recentpost)
 
-
-{{-- <ul class="flex" style="display:grid; grid-template-columns: 50% 50%;gap: 16px;"> --}}
-@foreach ($popularPosts as $post)
 <li>
-    <div class="popular-post-widget-item " >
+    <div class="popular-post-widget-item content_left_slider " >
         <div class="popular-post-widget-img" >
-            <a href="{{ route('ShoWarticle', ['id' => $post->id]) }}"><img src="{{ asset($post->IMG) }}" alt="#" style=""></a>
+            <a href="{{ route('ShoWarticle', ['id' => $recentpost->id]) }}"><img src="{{ asset($recentpost->IMG) }}" alt="#" style=""></a>
 
         </div>
-        <div class="popular-post-widget-brief">
-            <div class="ltn__blog-meta float-text">
+        <div class="popular-post-widget-brief float-text">
+            <div class="ltn__blog-meta ">
                 <ul>
                     <span class=" " style="width: 100%;
                     display: inline-block;
                     text-align: center;
-                    color:white"><a href="{{ route('ShoWarticle', ['id' => $post->id]) }}"> {{ $post->TITLE }}</a></span>
+                    color:white"><a href="{{ route('ShoWarticle', ['id' => $recentpost->id]) }}"> {{ $recentpost->TITLE }}</a></span>
                     <li>
                         <span></span>
-                        {{-- <span>{{ \Carbon\Carbon::parse($post->DATE_SCHEDULER)->locale('ar')->isoFormat('MMM DD, YYYY') }}</span> --}}
+                        {{-- <span>{{ \Carbon\Carbon::parse($recentpost->DATE_SCHEDULER)->locale('ar')->isoFormat('MMM DD, YYYY') }}</span> --}}
                     </li>
 
                 </ul>
@@ -26,6 +23,5 @@
         </div>
     </div>
 </li>
-
 @endforeach
 {{-- </ul> --}}
