6e848cbaa1da50b96d9d3a945faf8bd4b10e5c55
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-02 12:07:33 +0300

    tm

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -945,21 +945,7 @@ use App\Models\poststags;
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script>
-        var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
-    </script>
+
     <script> window.onload = function ()
     {
         // var desiredHeight = "calc(100vh - 218px)";
@@ -1018,5 +1004,18 @@ use App\Models\poststags;
     $(document).ready(function () {
         console.log($('.ajaxul').length); // Log the length of the selected element
         // Other code
+    });
+    var swiper = new Swiper('.blog-slider', {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
     });
 </script>
