3793a1b647e66a16e685cbc7a5bcc6203e6bea3e
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-04 15:55:39 +0300

    t

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1914,14 +1914,5 @@ fetchContent('صحة الطفل');
         }
     }, 3000);
 
-    $(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true
-});
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
-  });
-});
+   
 </script>
--- resources/views/partial/content_slider.blade.php
+++ resources/views/partial/content_slider.blade.php
@@ -3,7 +3,7 @@
     <div class="owl-carousel custom-carousel owl-theme">
 
         @foreach ($posts_content as $post)
-        
+
             <div class="item" style="background-image: url({{ $post->IMG }});">
                 <div class="item-desc">
                     <h3>{{ $post->TITLE }}</h3>
@@ -14,3 +14,15 @@
 
     </div>
 
+<script>
+     $(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
+</script>
