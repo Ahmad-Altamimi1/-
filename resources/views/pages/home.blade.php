cb7ff364752cbbcc843627c284955193a9741fd9
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-04 15:46:19 +0300

    t

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1723,7 +1723,10 @@ button {
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+    <!-- Add the Owl Carousel JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script> window.onload = function ()
     {
         // var desiredHeight = "calc(100vh - 218px)";
