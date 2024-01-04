3af4a7ab12b0e1de511eca05371aa65be3797a06
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-04 11:37:58 +0300

    t

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1219,8 +1219,8 @@ use App\Models\poststags;
 
 
     @endsection
-    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
     <script> window.onload = function ()
