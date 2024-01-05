161d249829aaa541540967d5f3e67f497dc84de5
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-05 17:31:18 +0300

    t

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1836,7 +1836,8 @@
         </div>
     @endsection
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
     <!-- Add the Owl Carousel JS -->
