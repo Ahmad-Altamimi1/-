b8dd0948607a918ab93d996a237970b20135e571
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-06 13:33:09 +0300

    t

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1601,13 +1601,15 @@ height: 100%;
 
                                             <div class="image"><img src="{{ asset($video->IMG) }}" /></div>
                                             <div class="text is-text-centered">
-                                                <a href="/tags/{{ $video->tag->id }}/show"
-                                                    class="category-badge lg">{{ $video->tag->TITLE }}</a>
+
                                                 <p style=" color: white;
                                                 height: 100%;
                                                 display: flex;
                                                 justify-content: center;
-                                                align-items: center;"> {{ $video->TITLE }}</p>
+                                                align-items: center;">
+                                                   <a href="/tags/{{ $video->tag->id }}/show"
+                                                    class="category-badge lg">{{ $video->tag->TITLE }}</a>
+                                                {{ $video->TITLE }}</p>
                                             </div>
 
                                         </div> @endforeach
