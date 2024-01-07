865f43edbe3d3dc9ba471439259369d39d34b18c
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-07 13:23:59 +0300

    t

--- resources/views/partial/content_slider.blade.php
+++ resources/views/partial/content_slider.blade.php
@@ -5,12 +5,11 @@
 <div class="slick-carousel custom-carousel contentslider">
 
     @foreach ($posts_content->take(12) as $post)
-        <div class="item" style="background-image: url({{ $post->IMG }});">
+        {{-- <div class="item" style="background-image: url({{ $post->IMG }});">
             <div class="item-desc">
                 <h3>{{ $post->TITLE }}</h3>
-                {{-- <p>{{ $post->DESCRIPTION}}</p> --}}
             </div>
-        </div>
+        </div> --}}
     @endforeach
 
 </div>
