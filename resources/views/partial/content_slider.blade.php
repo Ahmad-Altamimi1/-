ed2ea2d169778baa999805a3ccc308637808eba9
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-07 21:48:02 +0300

    tmtm

--- resources/views/partial/content_slider.blade.php
+++ resources/views/partial/content_slider.blade.php
@@ -1,6 +1,4 @@
-{{-- <h2 class="line-title">ف</h2> --}}
-<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
 
 
     @foreach ($posts_content->take(12) as $post)
@@ -12,26 +10,6 @@
     @endforeach
 
 
-{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  --}}
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="{{url('slick/slick.min.js')}}"></script>
 
-<script type="text/javascript"> 
-$(document).ready(function () {
-    console.log('Document ready');
-});
-
-  $(document).ready(function () {
-    console.log('Slick initialization script executed');
-      $('.slider-for').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          fade: true,
-      });
-  });
-  </script>
-  
 
 
