f81aea606ea080787297de8136f5af00357e9d70
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-08 00:14:43 +0300

    tmtm

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -52,7 +52,7 @@ $games=$recentposts;
         $left_side_bar_content= $this->getposts_in_one_tag('أعتني بطفلك');
         // $left_side_bar_content = poststags::where('TITLE','=','أعتني بطفلك')->first();
         $defaultPosts = Post::take(4)->get();
-
+            
         return view('pages.home',compact('selectedTag','recentposts','games', 'tags', 'Monthsofpregnancy', 'left_side_bar_content', 'defaultPosts', 'first_tag','newsList'));
     }
     public function getposts_in_one_tag($tagname){
