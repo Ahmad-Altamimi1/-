69d3e8a21a5cf7376322ad6d4d02c3cd0e6aac10
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-08 01:21:06 +0300

    tmtm

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1587,7 +1587,7 @@ height: 100%;
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <div class="bottom_content">
+    <div class="bottom_content container">
         <ul class="nav nav-tabs" id="myTabs" role="tablist">
             <li class="nav-item">
                 <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab"
