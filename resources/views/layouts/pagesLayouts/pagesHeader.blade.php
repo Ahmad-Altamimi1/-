039cddf1eb260f733d304c41eec21667a506e8e8
Ahmad-Altamimi1 <ahmad.altamimi5.1999@gmail.com>
2024-01-08 20:49:52 +0300

    tmtm

--- resources/views/layouts/pagesLayouts/pagesHeader.blade.php
+++ resources/views/layouts/pagesLayouts/pagesHeader.blade.php
@@ -172,7 +172,10 @@
                     <ul>
                         <li><a href="#" title="Facebook"><i class="icon-social-facebook"></i></a></li>
                         <li><a href="#" title="Twitter"><i class="icon-social-twitter"></i></a></li>
-                        <li><a href="#" title="Tiktok"><i class="fa-brands fa-tiktok"> <img src="{{asset('tiktok.svg')}}" alt=""></i></a></li>
+                        <li><a href="#" title="Tiktok"><i class="fa-brands fa-tiktok"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16" style="fill: white;">
+                            <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+                          </svg>
+                          </i></a></li>
                         <li><a href="#" title="Youtube"><i class="icon-social-youtube"></i></a></li>
                         <li><a href="#" title="Instagram"><i class="icon-social-instagram"></i></a></li>
                     </ul>
